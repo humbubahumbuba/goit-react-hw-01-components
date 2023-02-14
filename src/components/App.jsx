c13011d6f15d312ea23997fd2cfce59a7e28cac6
Paul Droshniev <108367655+humbubahumbuba@users.noreply.github.com>
2023-02-14 14:23:40 +0200

    stats

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,24 @@
-export const App = () => {
+import User from 'data-pack/user';
+import Profile from 'components/Profile';
+import Statistics from './statistics';
+import data from 'data-pack/data';
+
+export default function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        username={User.username}
+        tag={User.tag}
+        location={User.location}
+        avatar={User.avatar}
+        stats={User.stats}
+        followers={User.stats.followers}
+        views={User.stats.views}
+        likes={User.stats.likes}
+          />
+          
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
     </div>
   );
-};
+}
---
+++ src/components/statistics.js
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types';
+
+export default function Statistics({ title, stats }) {
+  return (
+    <section>
+      {title && <h2>{title}</h2>}
+      <ul>
+        {stats.map(({ id, label, percentage }) => {
+          return (
+            <li key={id}>
+              <span>{label}</span>
+              <span>{percentage}</span>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
+}
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ),
+};
